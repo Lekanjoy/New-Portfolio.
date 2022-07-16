cbaec6a0967ccec885b6e8fbac9e043855f531fa
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2022-07-16 23:19:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
+// Preloader
+  const preloader = document.getElementById("preloader");
+window.addEventListener("load", () => {
+  preloader.style.display = "none";
+});
+
 const hamburgerMenu = document.getElementById("menu");
 const navLinks = document.getElementById("nav-links");
 let navItem = document.querySelectorAll("li");
-let header = document.getElementById('header');
+let header = document.getElementById("header");
 
 hamburgerMenu.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
 
 window.addEventListener("click", (e) => {
-  if (e.target.id != "menu"/*  && e.target.localName != 'li' */) {
+  if (e.target.id != "menu" /*  && e.target.localName != 'li' */) {
     navLinks.classList.remove("show");
-    console.log(e);
   }
 });
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > header.offsetHeight + 400) {
-    header.classList.add('scroll'); 
-  }else{
-    header.classList.remove("scroll"); 
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
   }
-
-})
-
-
-const downloadBtn = document.getElementById('download');
-
-downloadBtn.addEventListener('click',() => {
-  downloadBtn.innerText = 'Downloading . . .';
-  
-  setTimeout(() => { 
-    downloadBtn.textContent = "Download CV"
-  },3000)
 });
 
-// Preloader
-window.addEventListener('load', () => {
-let preloader = document.getElementById("preloader");
+// Download CV
+const downloadBtn = document.getElementById("download");
 
-preloader.style.display = 'none';
+downloadBtn.addEventListener("click", () => {
+  downloadBtn.innerText = "Downloading . . .";
+
+  setTimeout(() => {
+    downloadBtn.textContent = "Download CV";
+  }, 3000);
 });
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "about.html", "services.html", "contact.html", 'projects.html'],
+  content: [
+    "index.html",
+    "about.html",
+    "services.html",
+    "contact.html",
+    "projects.html",
+  ],
   theme: {
     extend: {
       colors: {
-        LightCyan: "#f6f8fa",
-        NeonGreen: "hsl(150, 100%, 66%)",
-        GrayishBlue: "hsl(217, 19%, 38%)",
-        DarkGrayishBlue: "hsl(217, 19%, 24%)",
-        DarkBlue: "hsl(218, 23%, 16%)",
         primary: "#615CFD",
         contact: "#D465EF",
         bgProject: "#001D38",
-        project: "#dceaf5",
+        project: "#e0dcf566",
         footer: "#C7C7C7",
         welcome: "#844ce3",
-        lightBlack: '#000000f2',
+        lightBlack: "#000000f2",
       },
       fontFamily: {
         mulish: "Mulish",
@@ -25,6 +26,9 @@ module.exports = {
       },
       width: {
         mid: "35rem",
+      },
+      backgroundImage: {
+        preload: "url('/images/preloader.gif')",
       },
     },
   },
